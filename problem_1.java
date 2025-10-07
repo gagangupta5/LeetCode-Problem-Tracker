c2891ee587fe7fac86f2fc199660c258c96fd692
gagangupta5 <2k23.cscys2310911@gmail.com>
2025-10-07 12:41:31 +0000

    Two Sum

--- problem_1.java
+++ problem_1.java
@@ -1,23 +1,21 @@
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
-int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
-    int* result=(int*)malloc(2*sizeof(int));
-    *returnSize=2;
-    for(int i=0;i<numsSize;i++)
-    {
-        int x=target-nums[i];
-        for(int j=i+1;j<numsSize;j++)
-        {
-            if(x==nums[j])
-            {
-                result[0]=i;
-                result[1]=j;
-                return result;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        
+        for (int i = 0; i < nums.length; i++) {
+            int x = target - nums[i];
+            for (int j = i + 1; j < nums.length; j++) {
+                if (x == nums[j]) {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
             }
         }
+        
+        // Default return (if no pair found)
+        result[0] = 0;
+        result[1] = 1;
+        return result;
     }
-    result[0]=0;
-    result[1]=1;
-    return result;
 }
