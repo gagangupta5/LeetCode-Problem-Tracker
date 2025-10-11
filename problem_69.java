c4a5e34f7cee4839e1fb5de0cc3f6388f5e92c2a
gagangupta5 <2k23.cscys2310911@gmail.com>
2025-10-11 09:00:09 +0000

    Sqrt(x)

---
+++ problem_69.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int mySqrt(int x) {
+        int st = 1, end = x;
+        while (st <= end) {
+            int mid = st + (end - st) / 2;
+            if (mid * mid == x)
+                return mid;
+            else if ((long) mid * mid > (long) x)
+                end = mid - 1;
+            else {
+                st = mid + 1;
+            }
+        }
+        return Math.round(end);
+    }
+}
