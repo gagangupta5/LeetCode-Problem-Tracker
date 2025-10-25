07793a5caa5246e72733a303babfd4579a0d6df6
gagangupta5 <2k23.cscys2310911@gmail.com>
2025-10-25 16:03:22 +0000

    Calculate Money in Leetcode Bank

--- problem_1716.java
+++ problem_1716.java
@@ -1,3 +1,4 @@
+//Approach 1 (Brute Force)
 class Solution1 {
     public int totalMoney(int n) {
         int res=0;
@@ -16,5 +17,22 @@ class Solution1 {
             }
         }
         return res;
+    }
+}
+
+
+//Approach 2 (Optimised)
+class Solution {
+    public static int totalMoney(int n) {
+        int w = n/7;
+        int r = n % 7;
+        int sumFull = 28*w + 7*(w*(w-1)/2);
+        int SumRem = r*w + r*(r+1)/2;
+        return sumFull + SumRem;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(totalMoney(n));
     }
 }
