9a84aa205fd3f5092ae3c50407acd2b0141168a0
gagangupta5 <2k23.cscys2310911@gmail.com>
2025-11-11 13:37:02 +0000

    Sign of the Product of an Array

--- problem_1822.java
+++ problem_1822.java
@@ -1,4 +1,5 @@
-class Solution {
+//Approach 1 Time -> O(nlogn)
+class Solution1 {
     public int arraySign(int[] nums) {
         Arrays.sort(nums);
         int negCount=0;
@@ -9,5 +10,21 @@ class Solution {
         }
         if(negCount%2==0) return 1;
         return -1;
+    }
+}
+
+
+//Approach 2 (Optimised) Time -> O(n)
+class Solution {
+    public int arraySign(int[] nums) {
+        int n=nums.length;
+        int res=1;
+        for(int i=0;i<n;i++){
+            if(nums[i]==0) return 0;
+            else if(nums[i]<0){
+                res*=-1;
+            }
+        }
+        return res;
     }
 }
