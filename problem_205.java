7f6f2f6810bf6b8eb1f54d518085935e3b461e44
gagangupta5 <2k23.cscys2310911@gmail.com>
2025-11-12 06:46:10 +0000

    Isomorphic Strings

---
+++ problem_205.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        int[] mapS= new int[256];
+        int[] mapT= new int[256];
+        for(int i=0;i<s.length();i++){
+            char ch1=s.charAt(i);
+            char ch2=t.charAt(i);
+            if(mapS[ch1]==0 && mapT[ch2]==0){
+                mapS[ch1]=ch2;
+                mapT[ch2]=ch1;
+            }
+            else if(mapS[ch1]!=ch2) return false;
+        }
+        return true;
+    }
+}
