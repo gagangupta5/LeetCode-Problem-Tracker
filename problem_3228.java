6da2d9fc3107e54d4aea5c29acf6782896d36af8
gagangupta5 <2k23.cscys2310911@gmail.com>
2025-11-13 15:22:58 +0000

    Maximum Number of Operations to Move Ones to the End

---
+++ problem_3228.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int maxOperations(String s) {
+        int countOne = 0;
+        int res = 0;
+        int i = 0;
+        while (i < s.length()) {
+            if (s.charAt(i) == '0') {
+                while (i + 1 < s.length() && s.charAt(i + 1) == '0') {
+                    i++;
+                }
+                res += countOne;
+            } else {
+                countOne++;
+            }
+            i++;
+        }
+        return res;
+    }
+}
