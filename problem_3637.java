647e663a261f7e3c59e52c0b5c75387e5d7fdb10
gagangupta5 <2k23.cscys2310911@gmail.com>
2026-02-03 13:53:29 +0000

    Trionic Array I

---
+++ problem_3637.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isTrionic(int[] nums) {
+        int n=nums.length;
+        int i=1;
+        while(i<n && nums[i]>nums[i-1]) i++;
+        if(i==1 || i==n) return false;
+        while(i<n && nums[i]<nums[i-1]) i++;
+        if(i==n) return false;
+        while(i<n && nums[i]>nums[i-1]) i++;
+        return (i==n)?true:false;
+    }
+}
