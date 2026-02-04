37efa6350d4e013be19ce3b4b7bb82e9ca25bdad
gagangupta5 <2k23.cscys2310911@gmail.com>
2026-02-04 13:36:05 +0000

    Trionic Array II

---
+++ problem_3640.java
@@ -0,0 +1,34 @@
+class Solution {
+    public long maxSumTrionic(int[] nums) {
+        int n=nums.length;
+        long res=Long.MIN_VALUE;
+        for(int i=1;i<n-2;i++){
+            int a=i,b=i;
+            long net=nums[a];
+            while(b+1<n && nums[b+1]<nums[b]){
+                net+=(long)nums[b+1];
+                b++;
+            }
+            if(b==a) continue;
+            int c=b;
+            long left=0,right=0;
+            long lx=Long.MIN_VALUE;
+            long rx=Long.MIN_VALUE;
+            while(a-1>=0 && nums[a-1]<nums[a]){
+                left+=(long)nums[a-1];
+                lx=Math.max(lx,left);
+                a--;
+            }
+            if(a==i) continue;
+            while(b+1<n && nums[b+1]>nums[b]){
+                right+=(long)nums[b+1];
+                rx=Math.max(rx,right);
+                b++;
+            }
+            if(b==c) continue;
+            i=b-1;
+            res=Math.max(res,lx+rx+net);
+        }
+        return res;
+    }
+}
