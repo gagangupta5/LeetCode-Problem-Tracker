f27b4cd456bc0fcc308f30190dc67e57239f4713
gagangupta5 <2k23.cscys2310911@gmail.com>
2026-03-19 10:39:19 +0000

    Reverse Integer

---
+++ problem_7.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int reverse(int x) {
+        int res=0,d=0,n=Math.abs(x);
+        while(n!=0){
+            d=n%10;
+            if(res>(Integer.MAX_VALUE-d)/10) return 0;
+            res=res*10+d;
+            n/=10;
+        }
+        return (x<0)?-res:res;
+    }
+}
