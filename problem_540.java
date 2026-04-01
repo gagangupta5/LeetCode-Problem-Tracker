2c49517c0f99730900653412f2f3854a7e9648f6
gagangupta5 <2k23.cscys2310911@gmail.com>
2026-04-01 05:05:55 +0000

    Single Element in a Sorted Array

--- problem_540.java
+++ problem_540.java
@@ -3,20 +3,11 @@ class Solution {
         int n=nums.length;
         int l=0,h=n-1,mid=-1;
         if(n==1) return nums[0];
-        while(l<=h){
+        while(l<h){
             mid=(l+h)/2;
-            if(mid>0 && mid<n-1 && nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]) return nums[mid];
-            else if(mid==0 && nums[mid]!=nums[mid+1]) return nums[mid];
-            else if(mid==n-1 && nums[mid]!=nums[mid-1]) return nums[mid];
-            else if(nums[mid]==nums[mid-1]){
-                if(mid%2==0) h=mid-1;
-                else l=mid+1;
-            }
-            else {
-                if(mid%2==0) l=mid+1;
-                else h=mid-1;
-            }
+            if((mid%2!=0 && nums[mid]==nums[mid-1]) || (mid%2==0 && nums[mid]==nums[mid+1])) l=mid+1;
+            else h=mid;
         }
-        return -1;
+        return nums[l];
     }
 }
