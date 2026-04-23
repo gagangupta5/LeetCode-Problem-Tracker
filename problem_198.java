73074ed180164188ccec9365f7831b5c2a37ddff
gagangupta5 <2k23.cscys2310911@gmail.com>
2026-04-23 10:20:10 +0000

    House Robber

--- problem_198.java
+++ problem_198.java
@@ -1,4 +1,5 @@
-class Solution {
+//Solution 1: Brute Force
+class Solution1 {
     public int rob(int[] nums) {
         int n=nums.length;
         int s1=0,s2=0;
@@ -8,5 +9,23 @@ class Solution {
             s1=x;
         }
         return s1;
+    }
+}
+
+//Solution 2: Using Memoization
+class Solution {
+    public int rob(int[] nums) {
+        int[] memo = new int[nums.length];
+        java.util.Arrays.fill(memo, -1);
+        return solve(nums, 0, memo);
+    }
+
+    private int solve(int[] nums, int i, int[] memo) {
+        int n = nums.length;
+        if (i >= n) return 0;
+        if (memo[i] != -1) return memo[i];
+        int curr = nums[i] + solve(nums, i + 2, memo);
+        int skip = solve(nums, i + 1, memo);
+        return memo[i] = Math.max(curr, skip);
     }
 }
