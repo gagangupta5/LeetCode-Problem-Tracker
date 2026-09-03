fe7191d776f85b0106dee73302b573d6899a3e3e
gagangupta5 <2k23.cscys2310911@gmail.com>
2026-09-03 11:47:16 +0530

    Add Two Numbers

--- problem_2.java
+++ problem_2.java
@@ -10,56 +10,20 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode res=null;
-        ListNode newHead=res;
+        ListNode dummy=new ListNode(-1);
+        ListNode temp=dummy;
         int c=0;
-        while(l1!=null && l2!=null){
-            int sum=l1.val+l2.val+c;
-            int x=sum%10;
+        while(l1!=null || l2!=null || c>0){
+            int x=l1!=null ? l1.val : 0;
+            int y=l2!=null ? l2.val : 0;
+            int sum=x+y+c;
+            ListNode newNode=new ListNode(sum%10);
+            temp.next=newNode;
+            temp=newNode;
             c=sum/10;
-            ListNode temp=new ListNode(x);
-            if(res==null){
-                newHead=res=temp;
-            }
-            else{
-                res.next=temp;
-                res=res.next;
-            }
-            l1=l1.next;
-            l2=l2.next;
+            l1=l1!=null ? l1.next : l1;
+            l2=l2!=null ? l2.next : l2;
         }
-        while(l1!=null){
-            int sum=l1.val+c;
-            int x=sum%10;
-            c=sum/10;
-            ListNode temp=new ListNode(x);
-            if(res==null){
-                newHead=res=temp;
-            }
-            else{
-                res.next=temp;
-                res=res.next;
-            }
-            l1=l1.next;
-        }
-        while(l2!=null){
-            int sum=l2.val+c;
-            int x=sum%10;
-            c=sum/10;
-            ListNode temp=new ListNode(x);
-            if(res==null){
-                newHead=res=temp;
-            }
-            else{
-                res.next=temp;
-                res=res.next;
-            }
-            l2=l2.next;
-        }
-        if(c==1){
-            ListNode temp=new ListNode(c);
-            res.next=temp;
-        }
-        return newHead;
+        return dummy.next;
     }
 }
